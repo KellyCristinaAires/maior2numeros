6154d5e55c1057ae3b2b4ed27d5ff4ffdf612fbe
KellyCristinaAires <k.woys@hotmail.com>
2020-11-08 14:48:09 -0300

    atividade 2 números

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace maior2numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+            double MaiorValor = 0;
+
+            Console.Write("Digite o 1ºvalor: ");
+            valor = double.Parse(Console.ReadLine());
+            
+            if (valor > MaiorValor)
+            {
+               MaiorValor = valor;
+               
+            }
+                
+           Console.Write("Digite O 2ºvalor; ");
+           valor = double.Parse(Console.ReadLine());
+
+           if (valor > MaiorValor)
+           {
+           MaiorValor = valor;
+           }
+            
+          Console.WriteLine($" O maior numero digitado é: {MaiorValor}");
+        }
+    }
+}
